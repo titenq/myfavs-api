2fcafa076bd06e888c069488757f9cdf09c2e9b7
titenq <titenq@gmail.com>
2025-02-04 13:37:48 -0300

    errorHandler

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -3,7 +3,7 @@ import { ZodError } from 'zod';
 
 import { IGenericError } from '@/interfaces/errorInterface';
 
-const errorHandler = (error: ZodError | IGenericError, request: FastifyRequest, reply: FastifyReply) => {
+const errorHandler = (error: Error | ZodError | IGenericError, request: FastifyRequest, reply: FastifyReply) => {
   if (error instanceof ZodError) {
     reply.status(400).send({
       error: true,
@@ -14,9 +14,17 @@ const errorHandler = (error: ZodError | IGenericError, request: FastifyRequest, 
     return;
   }
 
-  const statusCode = error?.statusCode || 500;
+  if ('statusCode' in error) {
+    reply.status(error.statusCode).send(error);
 
-  reply.status(statusCode).send(error);
+    return;
+  }
+
+  reply.status(500).send({
+    error: true,
+    message: error.message || 'Internal Server Error',
+    statusCode: 500
+  })
 };
 
 export default errorHandler;
